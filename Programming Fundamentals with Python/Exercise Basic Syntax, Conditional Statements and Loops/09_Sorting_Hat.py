fe534587a64597a8a642365389854f6644e8fb53
Lubcata <lflorov2@gmail.com>
2023-09-20 11:52:35 +0300

    Update Sorting_Hat and Easter_Bread

--- Programming Fundamentals with Python/Exercise Basic Syntax, Conditional Statements and Loops/09_Sorting_Hat.py
+++ Programming Fundamentals with Python/Exercise Basic Syntax, Conditional Statements and Loops/09_Sorting_Hat.py
@@ -1,21 +1,21 @@
-budget = float(input())
-price_kg_flour = float(input())
-
-price_pack_eggs = price_kg_flour * 0.75
-price_quarter_milk = (price_kg_flour * 1.25) * 0.25
-
-total_price_loaf = price_kg_flour + price_pack_eggs + price_quarter_milk
-
-loaves_total = budget // total_price_loaf
-
-colored_eggs = 0
-loaves_counter = 0
-
-for i in range(1, int(loaves_total) + 1):
-    colored_eggs += 3
-    loaves_counter += 1
-    if i % 3 == 0:
-        colored_eggs -= (loaves_counter - 2)
-
-print(f"You made {int(loaves_total)} loaves of Easter bread! Now you have {colored_eggs} eggs "
-      f"and {budget - (total_price_loaf * loaves_total):.2f}BGN left.")
+command = input()
+hats_are_sorted = True
+while command != "Welcome!":
+    current_name = command
+    current_house = ""
+    if current_name == "Voldemort":
+        hats_are_sorted = False
+        print("You must not speak of that name!")
+        break
+    else:
+        if len(current_name) < 5:
+            print(f"{current_name} goes to Gryffindor.")
+        elif len(current_name) == 5:
+            print(f"{current_name} goes to Slytherin.")
+        elif len(current_name) == 6:
+            print(f"{current_name} goes to Ravenclaw.")
+        else:
+            print(f"{current_name} goes to Hufflepuff.")
+    command = input()
+if hats_are_sorted:
+    print("Welcome to Hogwarts.")
