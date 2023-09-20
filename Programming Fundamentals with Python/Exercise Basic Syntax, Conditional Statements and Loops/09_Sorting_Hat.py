775f4c1aa91c23492a97e19dbd6d73fb6d4f99ee
Lubcata <lflorov2@gmail.com>
2023-09-20 11:32:39 +0300

    Final sulution

--- Programming Fundamentals with Python/Exercise Basic Syntax, Conditional Statements and Loops/09_Sorting_Hat.py
+++ Programming Fundamentals with Python/Exercise Basic Syntax, Conditional Statements and Loops/09_Sorting_Hat.py
@@ -1,11 +1,21 @@
-# Eggs	1 pack
-# Flour	1 kg
-# Milk	0.250 l
 budget = float(input())
-flour_price = float(input())
-eggs_price = flour_price * 0.75
-milk_price = flour_price + flour_price * 0.25
-colored_eggs = 0
+price_kg_flour = float(input())
 
-while budget <= 0:
-    pass
+price_pack_eggs = price_kg_flour * 0.75
+price_quarter_milk = (price_kg_flour * 1.25) * 0.25
+
+total_price_loaf = price_kg_flour + price_pack_eggs + price_quarter_milk
+
+loaves_total = budget // total_price_loaf
+
+colored_eggs = 0
+loaves_counter = 0
+
+for i in range(1, int(loaves_total) + 1):
+    colored_eggs += 3
+    loaves_counter += 1
+    if i % 3 == 0:
+        colored_eggs -= (loaves_counter - 2)
+
+print(f"You made {int(loaves_total)} loaves of Easter bread! Now you have {colored_eggs} eggs "
+      f"and {budget - (total_price_loaf * loaves_total):.2f}BGN left.")
