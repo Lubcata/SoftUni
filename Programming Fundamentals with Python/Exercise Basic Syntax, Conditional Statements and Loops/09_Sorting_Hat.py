5a8546dbfed8b25a81243da7147f2c444ec22b96
Lubcata <lflorov2@gmail.com>
2023-09-20 11:23:13 +0300

    Update 09_Sorting_Hat.py

---
+++ Programming Fundamentals with Python/Exercise Basic Syntax, Conditional Statements and Loops/09_Sorting_Hat.py
@@ -0,0 +1,11 @@
+# Eggs	1 pack
+# Flour	1 kg
+# Milk	0.250 l
+budget = float(input())
+flour_price = float(input())
+eggs_price = flour_price * 0.75
+milk_price = flour_price + flour_price * 0.25
+colored_eggs = 0
+
+while budget <= 0:
+    pass
