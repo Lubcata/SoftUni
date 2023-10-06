a7f763b01ae4566ede88b1dfabb9e30d0808bf7a
Lubcata <lflorov2@gmail.com>
2023-10-06 23:03:29 +0300

    Update ex. 02

--- Programming Fundamentals with Python/Exercise Functions/02_Add_and_Subtract.py
+++ Programming Fundamentals with Python/Exercise Functions/02_Add_and_Subtract.py
@@ -1,12 +1,4 @@
-def smallest(some_numbers: list):
-    return min(some_numbers)
-
-
-first_number = int(input())
-second_number = int(input())
-third_number = int(input())
-smallest_element = smallest([first_number, second_number, third_number])
-print(smallest_element)def sum_numbers(first, second):
+def sum_numbers(first, second):
     return first + second
 
 
