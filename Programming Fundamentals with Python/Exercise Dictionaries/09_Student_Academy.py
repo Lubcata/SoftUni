513f6eb76d8eb05788ecce38b8a7a1a8e50a6690
Lubcata <lflorov2@gmail.com>
2023-11-08 22:24:15 +0200

    Complete Exercise Dictionary

--- Programming Fundamentals with Python/Exercise Dictionaries/09_Student_Academy.py
+++ Programming Fundamentals with Python/Exercise Dictionaries/09_Student_Academy.py
@@ -1,10 +1,15 @@
-
 count_of_students = int(input())
 students = {}
 for student in range(count_of_students):
     student_name = input()
     student_grade = float(input())
-    if student not in students:
-        students[student_name] = 0
-    students[student_name] += student_grade
+    if student_name not in students:
+        students[student_name] = []
+    students[student_name].append(student_grade)
+
+for student, grades in students.items():
+    average_grade = sum(grades) / len(grades)
+    if average_grade >= 4.50:
+        print(f"{student} -> {average_grade:.2f}")
+        
 
